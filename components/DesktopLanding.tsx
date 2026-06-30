bcefb1c3aa27df6526c30df1692d1a154dd10583
Andrey <a.vershinin.21vek.by@gmail.com>
2026-06-30 21:10:49 +0300

    Remove blocking Google Fonts import

--- components/DesktopLanding.tsx
+++ components/DesktopLanding.tsx
@@ -199,7 +199,7 @@ export default function DesktopLanding() {
   return (
     <>
       <style>{`
-        @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800;900&display=swap');
+        
 
         :root {
           --bg: #050607;
--- components/MobileLanding.tsx
+++ components/MobileLanding.tsx
@@ -269,7 +269,7 @@ export default function MobileLanding() {
   return (
     <main className="tm" id="top">
       <style>{`
-        @import url('https://fonts.googleapis.com/css2?family=Manrope:wght@400;500;600;700;800;900&display=swap');
+        
 
         :root {
           --tm-bg: #050607;
