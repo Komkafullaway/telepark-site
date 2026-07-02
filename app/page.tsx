b678a0d7153bc8747d1287560a5679c93aab331b
Komkafullaway <ddzkg79rb4@privaterelay.appleid.com>
2026-07-02 15:03:41 +0300

    Improve hero image sizes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-"use client";
-
-import { useState } from "react";
 import DesktopLanding from "../components/DesktopLanding";
 import MobileLanding from "../components/MobileLanding";
 
 export default function Home() {
-  const [leadModalOpen, setLeadModalOpen] = useState(false);
-
   return (
     <>
       <div className="desktopOnly">
