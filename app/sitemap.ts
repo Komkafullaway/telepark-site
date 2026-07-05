39abc912a24913437fc3ccc4f2b5bf0b269d1bc3
Andrey <a.vershinin.21vek.by@gmail.com>
2026-07-05 17:55:25 +0300

    Update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,30 +8,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "daily",
       priority: 1,
     },
-    {
-      url: "https://tpark.ru/privacy",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.3,
-    },
-    {
-      url: "https://tpark.ru/consent",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.3,
-    },
-  ];
-}import { MetadataRoute } from "next";
-
-export const dynamic = "force-dynamic";
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: "https://tpark.ru/",
-      lastModified: new Date(),
-      changeFrequency: "daily",
-      priority: 1,
-    },
   ];
 }
